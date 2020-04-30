501522b5701f568c65f9f0065800edbd15082cc2
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-05-01 00:08:51 +0530

    Complete artifact functionality

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,5 @@
 class Artifact < ApplicationRecord
+  before_save :upload_to_s3
   belongs_to :project
   attr_accessor :upload
   
@@ -13,5 +14,13 @@ class Artifact < ApplicationRecord
       errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
     end
   end
-  
+
+  def upload_to_s3
+    s3 = Aws::S3::Resource.new
+    tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    obj.upload_file(upload.path, acl:'public-read')
+    self.key = obj.public_url
+  end
+
 end
